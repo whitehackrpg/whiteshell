0869409348db4eed3b4b994c07c7799528ef02b6
Christian Mehrstam <130791778+whitehackrpg@users.noreply.github.com>
2023-04-15 20:28:56 +0200

    Add files via upload

--- random-list-item.lisp
+++ random-list-item.lisp
@@ -1,4 +1,5 @@
 (defun random-item (input &optional pretty)
+  "Pick a random item either from a file with a list or an actual List list."
   (let* ((thelist (if (listp input) input (uiop:read-file-lines input)))
 	 (pick (nth (random (length thelist)) thelist)))
     (if pretty (format t "~a" pick) pick)))
