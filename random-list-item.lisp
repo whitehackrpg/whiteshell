3a5a1ab17bda262649bb01b6a3f22f6d0285efc3
Christian Mehrstam <130791778+whitehackrpg@users.noreply.github.com>
2023-04-15 20:31:09 +0200

    Update random-list-item.lisp

--- random-list-item.lisp
+++ random-list-item.lisp
@@ -1,5 +1,5 @@
 (defun random-item (input &optional pretty)
-  "Return or print a random item either from a file with a list or an actual List list."
+  "Return or print a random item either from a file with a list or an actual Lisp list."
   (let* ((thelist (if (listp input) input (uiop:read-file-lines input)))
 	 (pick (nth (random (length thelist)) thelist)))
     (if pretty (format t "~a" pick) pick)))
