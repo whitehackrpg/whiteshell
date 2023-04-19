82d13cb8ceb27d7b8a447bab0174bcc98d3772dc
Christian Mehrstam <130791778+whitehackrpg@users.noreply.github.com>
2023-04-19 13:15:11 +0200

    Delete random-list-item.lisp

--- random-list-item.lisp
+++
@@ -1,6 +0,0 @@
-(defun random-item (input &optional pretty)
-  "Return or print a random item either from a file with a list or an actual Lisp list."
-  (let* ((thelist (if (listp input) input (uiop:read-file-lines input)))
-	 (pick (nth (random (length thelist)) thelist)))
-    (if pretty (format t "~a" pick) pick)))
-    
